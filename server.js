ae7db75c94e4643d1555372f790c8ab207a67279
GulenayMer <mervegulenay@gmail.com>
2023-07-28 22:30:37 +0200

    api key

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
-require('dotenv/config');
+//require('dotenv/config');
+require("dotenv").config();
 const { Pool } = require('pg');
 const cors = require('cors');
+const axios = require('axios');
 
 // instantiations
 const app = express();
@@ -10,13 +12,42 @@ const pool = new Pool({
 	connectionString: process.env.ELEPHANT_SQL_CONNECTION_STRING,
 })
 
+
+
 // usage
 app.use(express.json());
 app.use(cors());
 
+const baseUrl = "https://api.themoviedb.org/3";
+const API_KEY = process.env.API_KEY;
+
+/* get API data */
+app.get('/api/movies', async (req, res) => {
+	try {
+	const response = await axios.get(`${baseUrl}/movie/popular?api_key=${API_KEY}`);
+	const movies = response.data.results;
+		//console.log(movies);
+	// Save the movie data to the database
+	for (const movie of movies) {
+		await pool.query(
+		"INSERT INTO movies (id, title, director, year, rating, poster, description) VALUES ($1, $2, $3, $4, $5, $6, $7)",
+		[movie.id, movie.title, "Ben Sth", movie.release_date, movie.vote_average, movie.poster_path, movie.overview]
+		);
+	}
+
+	res.json(movies);
+	} catch (error) {
+	console.error("Error fetching data: ", error);
+	res.status(500).json({ error: "Failed to fetch movie data" });
+}
+});
+
+
+
+
 /* -------- DATA --------- */
 // 1) GET ALL MOVIES
-app.get('/api/movies', (req,res) =>{
+app.get('/api/moviesdb', (req,res) =>{
 	pool
 		.query('SELECT * FROM movies;')
 		.then(data => {
@@ -28,7 +59,7 @@ app.get('/api/movies', (req,res) =>{
 });
 
 // 2) GET SINGLE MOVIE
-app.get(`/api/movies/:id`, (req,res) =>{
+app.get(`/api/moviesdb/:id`, (req,res) =>{
 	const { id }= req.params;
 	const query = 'SELECT * FROM movies WHERE id=$1';
 	pool
@@ -46,27 +77,27 @@ app.get(`/api/movies/:id`, (req,res) =>{
 });
 
 // 3) POST
-app.post('/api/movies', (req,res) => {
-	const { title, director, year, rating, poster } = req.body;
-	const query = 'INSERT INTO movies (title, director, year, rating, poster ) VALUES ($1, $2, $3, $4, $5) RETURNING *';
+app.post('/api/moviesdb', (req,res) => {
+	const { title, director, year, rating, poster, description } = req.body;
+	const query = 'INSERT INTO movies (title, director, year, rating, poster, description) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
 	pool
-		.query(query, [title, director, year, rating, poster])
+		.query(query, [title, director, year, rating, poster, description])
 		.then(data => res.status(201).json(data.rows[0]))
 		.catch( e => res.status(500).json({message: e.message}));
 });
 
 // 4) PUT
-app.put('/api/movies/:id', (req, res) => {
+app.put('/api/moviesdb/:id', (req, res) => {
 	const { id } = req.params;
-	const { title, director, year, rating, poster } = req.body;
-	const query = 'UPDATE movies SET title =$1, director=$2, year=$3, rating=$4, poster=$5 WHERE id=$6 RETURNING *';
+	const { title, director, year, rating, poster, description } = req.body;
+	const query = 'UPDATE movies SET title =$1, director=$2, year=$3, rating=$4, poster=$5 description=$6 WHERE id=$6 RETURNING *';
 	pool
-		.query(query, [id, title, director, year, rating, poster])
+		.query(query, [id, title, director, year, rating, poster, description])
 		.then(data => res.status(202).json(data.rows[0]))
 		.catch(e => res.status(500).json({message:e.message}));
 })
 
-app.delete('/api/movies/:id', (req, res) => {
+app.delete('/api/moviesdb/:id', (req, res) => {
 	const { id } = req.params;
 	const query = 'DELETE FROM movies WHERE id=$1 RETURNING *;'
 	pool
